a58fa17c14cb39e59b0c9970689bee8e948348b5
evandrofr <38050761+evandrofr@users.noreply.github.com>
2020-03-12 10:50:17 -0300

    Pause buttom

--- src/main.c
+++ src/main.c
@@ -9,49 +9,67 @@
 #define LED_PIO_IDX       8                    // ID do LED no PIO
 #define LED_PIO_IDX_MASK  (1 << LED_PIO_IDX)   // Mascara para CONTROLARMOS o LED
 
+#define LED1_PIO           PIOC                 
+#define LED1_PIO_ID        12                  
+#define LED1_PIO_IDX       19                   
+#define LED1_PIO_IDX_MASK  (1 << LED1_PIO_IDX)  
+
+#define LED2_PIO           PIOD                
+#define LED2_PIO_ID        13                  
+#define LED2_PIO_IDX       26                  
+#define LED2_PIO_IDX_MASK  (1 << LED2_PIO_IDX) 
+
+#define LED3_PIO           PIOD                 
+#define LED3_PIO_ID        13                  
+#define LED3_PIO_IDX       11                   
+#define LED3_PIO_IDX_MASK  (1 << LED3_PIO_IDX)  
 
 #define BUT_PIO				PIOA
 #define BUT_PIO_ID			10
 #define BUT_PIO_IDX			11
 #define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX)
 
+
+#define BUT1_PIO				PIOA
+#define BUT1_PIO_ID				10
+#define BUT1_PIO_IDX			2
+#define BUT1_PIO_IDX_MASK (1u << BUT1_PIO_IDX)
+
+#define BUT2_PIO				PIOC
+#define BUT2_PIO_ID				12
+#define BUT2_PIO_IDX			13
+#define BUT2_PIO_IDX_MASK (1u << BUT2_PIO_IDX)
+
+#define BUT3_PIO				PIOD
+#define BUT3_PIO_ID				13
+#define BUT3_PIO_IDX			30
+#define BUT3_PIO_IDX_MASK (1u << BUT3_PIO_IDX)
+
+
 #define BUZ_PIO				PIOB
 #define BUZ_PIO_ID			11
 #define BUZ_PIO_IDX			2
 #define BUZ_PIO_IDX_MASK (1u << BUZ_PIO_IDX)
 
-/*  Default pin configuration (no attribute). */
-#define _PIO_DEFAULT             (0u << 0)
-/*  The internal pin pull-up is active. */
-#define _PIO_PULLUP              (1u << 0)
-/*  The internal glitch filter is active. */
-#define _PIO_DEGLITCH            (1u << 1)
-/*  The pin is open-drain. */
-#define _PIO_OPENDRAIN           (1u << 2)
-/*  The internal debouncing filter is active. */
-#define _PIO_DEBOUNCE            (1u << 3)
-/************************************************************************/
-/* constants                                                            */
-/************************************************************************/
-
-/************************************************************************/
-/* variaveis globais                                                    */
-/************************************************************************/
-
-/************************************************************************/
-/* prototypes                                                           */
-/************************************************************************/
-
 void init(void);
 void tone(int freq, int dur);
+void but1_callback(void);
+void but1_callback(void);
+void but2_callback(void);
+void but3_callback(void);
 /************************************************************************/
 /* interrupcoes                                                         */
 /************************************************************************/
 
 
-const int buzzer = 10; //Define pin 10, can use other PWM pins  (5,6 or 9)
-                       //Note pins 3 and 11 can't be used when using the tone function in Arduino Uno
-const int songspeed = 1.5; //Change to 2 for a slower version of the song, the bigger the number the slower the song
+const int buzzer = 10; 
+const int songspeed = 1.5;
+
+volatile char but_flag = 0;
+volatile char but1_flag = 0;
+volatile char but2_flag = 0;
+volatile char but3_flag = 0; 
+char pause = 1;
 //*****************************************
 #define NOTE_C4  262   //Defining note frequency
 #define NOTE_D4  294
@@ -69,123 +87,136 @@ const int songspeed = 1.5; //Change to 2 for a slower version of the song, the b
 #define NOTE_B5  988
 //*****************************************
 int notes[] = {       //Note of the song, 0 is a rest/pulse
-   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-   NOTE_A4, NOTE_G4, NOTE_A4, 0,
-   
-   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-   NOTE_A4, NOTE_G4, NOTE_A4, 0,
-   
-   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
-   NOTE_A4, NOTE_C5, NOTE_D5, NOTE_D5, 0, 
-   NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, 0,
-   NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4, 0,
-   
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_D5, NOTE_E5, NOTE_A4, 0, 
-   NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
-   NOTE_C5, NOTE_A4, NOTE_B4, 0,
+	NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
+	NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
+	NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
+	NOTE_A4, NOTE_G4, NOTE_A4, 0,
+	
+	NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
+	NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
+	NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
+	NOTE_A4, NOTE_G4, NOTE_A4, 0,
+	
+	NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
+	NOTE_A4, NOTE_C5, NOTE_D5, NOTE_D5, 0,
+	NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, 0,
+	NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4, 0,
+	
+	NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
+	NOTE_D5, NOTE_E5, NOTE_A4, 0,
+	NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
+	NOTE_C5, NOTE_A4, NOTE_B4, 0,
 
-   NOTE_A4, NOTE_A4, 
-   //Repeat of first part
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-   NOTE_A4, NOTE_G4, NOTE_A4, 0,
+	NOTE_A4, NOTE_A4,
+	//Repeat of first part
+	NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
+	NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
+	NOTE_A4, NOTE_G4, NOTE_A4, 0,
 
-   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-   NOTE_A4, NOTE_G4, NOTE_A4, 0,
-   
-   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
-   NOTE_A4, NOTE_C5, NOTE_D5, NOTE_D5, 0, 
-   NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, 0,
-   NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4, 0,
-   
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_D5, NOTE_E5, NOTE_A4, 0, 
-   NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
-   NOTE_C5, NOTE_A4, NOTE_B4, 0,
-   //End of Repeat
+	NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
+	NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
+	NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
+	NOTE_A4, NOTE_G4, NOTE_A4, 0,
+	
+	NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
+	NOTE_A4, NOTE_C5, NOTE_D5, NOTE_D5, 0,
+	NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, 0,
+	NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4, 0,
+	
+	NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
+	NOTE_D5, NOTE_E5, NOTE_A4, 0,
+	NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
+	NOTE_C5, NOTE_A4, NOTE_B4, 0,
+	//End of Repeat
 
-   NOTE_E5, 0, 0, NOTE_F5, 0, 0,
-   NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
-   NOTE_D5, 0, 0, NOTE_C5, 0, 0,
-   NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4,
+	NOTE_E5, 0, 0, NOTE_F5, 0, 0,
+	NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
+	NOTE_D5, 0, 0, NOTE_C5, 0, 0,
+	NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4,
 
-   NOTE_E5, 0, 0, NOTE_F5, 0, 0,
-   NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
-   NOTE_D5, 0, 0, NOTE_C5, 0, 0,
-   NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4
+	NOTE_E5, 0, 0, NOTE_F5, 0, 0,
+	NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
+	NOTE_D5, 0, 0, NOTE_C5, 0, 0,
+	NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4
 };
 //*****************************************
 int duration[] = {         //duration of each note (in ms) Quarter Note is set to 250 ms
-  125, 125, 250, 125, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 375, 125, 
-  
-  125, 125, 250, 125, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 375, 125, 
-  
-  125, 125, 250, 125, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 125, 250, 125,
+	125, 125, 250, 125, 125,
+	125, 125, 250, 125, 125,
+	125, 125, 250, 125, 125,
+	125, 125, 375, 125,
+	
+	125, 125, 250, 125, 125,
+	125, 125, 250, 125, 125,
+	125, 125, 250, 125, 125,
+	125, 125, 375, 125,
+	
+	125, 125, 250, 125, 125,
+	125, 125, 250, 125, 125,
+	125, 125, 250, 125, 125,
+	125, 125, 125, 250, 125,
 
-  125, 125, 250, 125, 125, 
-  250, 125, 250, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 375, 375,
+	125, 125, 250, 125, 125,
+	250, 125, 250, 125,
+	125, 125, 250, 125, 125,
+	125, 125, 375, 375,
 
-  250, 125,
-  //Rpeat of First Part
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 375, 125, 
-  
-  125, 125, 250, 125, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 375, 125, 
-  
-  125, 125, 250, 125, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 125, 250, 125,
+	250, 125,
+	//Rpeat of First Part
+	125, 125, 250, 125, 125,
+	125, 125, 250, 125, 125,
+	125, 125, 375, 125,
+	
+	125, 125, 250, 125, 125,
+	125, 125, 250, 125, 125,
+	125, 125, 250, 125, 125,
+	125, 125, 375, 125,
+	
+	125, 125, 250, 125, 125,
+	125, 125, 250, 125, 125,
+	125, 125, 250, 125, 125,
+	125, 125, 125, 250, 125,
 
-  125, 125, 250, 125, 125, 
-  250, 125, 250, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 375, 375,
-  //End of Repeat
-  
-  250, 125, 375, 250, 125, 375,
-  125, 125, 125, 125, 125, 125, 125, 125, 375,
-  250, 125, 375, 250, 125, 375,
-  125, 125, 125, 125, 125, 500,
+	125, 125, 250, 125, 125,
+	250, 125, 250, 125,
+	125, 125, 250, 125, 125,
+	125, 125, 375, 375,
+	//End of Repeat
+	
+	250, 125, 375, 250, 125, 375,
+	125, 125, 125, 125, 125, 125, 125, 125, 375,
+	250, 125, 375, 250, 125, 375,
+	125, 125, 125, 125, 125, 500,
 
-  250, 125, 375, 250, 125, 375,
-  125, 125, 125, 125, 125, 125, 125, 125, 375,
-  250, 125, 375, 250, 125, 375,
-  125, 125, 125, 125, 125, 500
+	250, 125, 375, 250, 125, 375,
+	125, 125, 125, 125, 125, 125, 125, 125, 375,
+	250, 125, 375, 250, 125, 375,
+	125, 125, 125, 125, 125, 500
 };
 
 void tone(int freq, int dur){
-	int T = 1000000/(2*freq); 
+	int T = 1000000/(2*freq);
 	int idx = (dur * freq)/1000;
 	for(int i = 0; i <= idx; i++){
-		pio_set(PIOB, BUZ_PIO_IDX_MASK);      // Coloca 1 no pino LED
-		delay_us(T);                        // Delay por software de 200 ms
-		pio_clear(PIOB, BUZ_PIO_IDX_MASK);    // Coloca 0 no pino do LED
+		pio_set(PIOB, BUZ_PIO_IDX_MASK);    
+		delay_us(T);                        
+		pio_clear(PIOB, BUZ_PIO_IDX_MASK); 
 		delay_us(T);
 	}
 }
+// CALLBACK
+void but_callback(void){
+	but_flag = 1;
+};
+void but1_callback(void){
+	but1_flag = 1;
+	};
+void but2_callback(void){
+	but2_flag = 1;
+};
+void but3_callback(void){
+	but3_flag = 1;
+};
 
 
 // Função de inicialização do uC
@@ -199,46 +230,89 @@ void init(void){
 	// Ativa o PIO na qual o LED foi conectado
 	// para que possamos controlar o LED.
 	pmc_enable_periph_clk(BUZ_PIO_ID);
+	pmc_enable_periph_clk(LED_PIO_ID);
+	pmc_enable_periph_clk(LED1_PIO_ID);
+	pmc_enable_periph_clk(LED2_PIO_ID);
+	pmc_enable_periph_clk(LED3_PIO_ID);
+	
+	pio_configure(LED1_PIO, PIO_OUTPUT_0, LED1_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_configure(LED2_PIO, PIO_OUTPUT_0, LED2_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_configure(LED3_PIO, PIO_OUTPUT_0, LED3_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_configure(LED_PIO , PIO_OUTPUT_0, LED_PIO_IDX_MASK, PIO_DEFAULT);
+	
 	pio_set_output(BUZ_PIO, BUZ_PIO_IDX_MASK, 0, 0, 0);
 	
 	// Inicializa PIO do botao
 	pmc_enable_periph_clk(BUT_PIO_ID);
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	pmc_enable_periph_clk(BUT2_PIO_ID);
+	pmc_enable_periph_clk(BUT3_PIO_ID);
+	
+	pio_configure(BUT_PIO , PIO_INPUT, BUT_PIO_IDX_MASK, PIO_PULLUP);
+	pio_configure(BUT1_PIO, PIO_INPUT, BUT_PIO_IDX_MASK, PIO_PULLUP);
+	pio_configure(BUT2_PIO, PIO_INPUT, BUT_PIO_IDX_MASK, PIO_PULLUP);
+	pio_configure(BUT3_PIO, PIO_INPUT, BUT_PIO_IDX_MASK, PIO_PULLUP);
 	
 	
+	pio_handler_set(BUT_PIO , BUT_PIO_ID, BUT_PIO_IDX_MASK, PIO_IT_FALL_EDGE, but_callback);	
+	pio_handler_set(BUT1_PIO, BUT1_PIO_ID, BUT1_PIO_IDX_MASK, PIO_IT_FALL_EDGE, but1_callback);	
+	pio_handler_set(BUT2_PIO, BUT2_PIO_ID, BUT2_PIO_IDX_MASK, PIO_IT_FALL_EDGE, but2_callback);	
+	pio_handler_set(BUT3_PIO, BUT3_PIO_ID, BUT3_PIO_IDX_MASK, PIO_IT_FALL_EDGE, but3_callback);	
 	
-	// configura pino ligado ao botão como entrada com um pull-up.
-	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_DEFAULT);
-	pio_pull_up(BUT_PIO,BUT_PIO_IDX_MASK,1);
-
+	NVIC_EnableIRQ(BUT_PIO_ID);
+	NVIC_SetPriority(BUT_PIO_ID, 2);
+	
+	NVIC_EnableIRQ(BUT1_PIO_ID);
+	NVIC_SetPriority(BUT1_PIO_ID, 2);
+	
+	NVIC_EnableIRQ(BUT2_PIO_ID);
+	NVIC_SetPriority(BUT2_PIO_ID, 3);
+	
+	NVIC_EnableIRQ(BUT3_PIO_ID);
+	NVIC_SetPriority(BUT3_PIO_ID, 2);
+	
+	pio_enable_interrupt(BUT1_PIO, BUT1_PIO_IDX_MASK);
+	pio_enable_interrupt(BUT2_PIO, BUT2_PIO_IDX_MASK);
+	pio_enable_interrupt(BUT3_PIO, BUT3_PIO_IDX_MASK);
 }
-
 
 int main(void) {
 	init();
-	int go;
-	// super loop
-	// aplicacoes embarcadas não devem sair do while(1).
+	int i = 0;
 	while (1) {
-		
-		go = pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK);
-		if(!go){
-			for (int i=0;i < sizeof(notes);i++){
-				int adj = 2;
-				int wait = duration[i] * songspeed/adj;
-			
-				
-				tone(notes[i],duration[i]/adj);      // Coloca 1 no pino
-				delay_ms(wait);                        // Delay
-				pio_clear(PIOB, BUZ_PIO_IDX_MASK);    // Coloca 0 no pino
-				delay_ms(wait);                        // Delay
-				
-			}
-			 //delay is used so it doesn't go to the next loop before tone is finished playing
-			//You can click reset on Arduino to replay the song
-
-			} else {
-			pio_set(PIOB, BUZ_PIO_IDX_MASK);
+		if(pause){
+			pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);	
 		}
+		
+		if(but2_flag) {
+			pause = !pause;
+			but2_flag = 0;	
+		}
+		
+		if(!pause){
+		//for (int i=0;i < sizeof(notes);i++){
+			int adj = 2;
+			int wait = duration[i] * songspeed/adj;
+				
+			tone(notes[i],duration[i]/adj);
+			delay_ms(wait);
+			pio_clear(PIOB, BUZ_PIO_IDX_MASK);
+			delay_ms(wait);
+				
+			but1_flag = 0;
+			if(i <sizeof(notes)) i++;
+			else {
+				i = 0;
+				pause = 0;
+			}
+		//}	
+			
+		} 
+// 		else {
+// 			pio_set(PIOB, BUZ_PIO_IDX_MASK);
+// 			
+// 		}
+		
 		
 	}
 	return 0;
